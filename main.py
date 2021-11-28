439c4ef5a84a1bf45e7ae5a3999c4f22994083ba
HassanKhan96 <khanhassan057@gmail.com>
2021-11-28 21:01:44 +0500

    init

--- main.py
+++ main.py
@@ -3,17 +3,17 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from Model import loadModel, prepareData
+# from Model import loadModel, prepareData
 from WebScapper import get_reviews
 from flask import Flask, jsonify, request
-import numpy as np
+# import numpy as np
 
 
 app = Flask(__name__)
 
 app.config["DEBUG"] = True
 
-model = loadModel()
+# model = loadModel()
 
 def detectFakeReviews(url = None):
 
@@ -22,32 +22,32 @@ def detectFakeReviews(url = None):
 
     #getting all reviews from scrapping.
     rawData = get_reviews(url)
-
+    return rawData
     #prepare data
-    reviewData = prepareData(rawData)
+    # reviewData = prepareData(rawData)
 
     #predict on data
-    pred = model.predict(reviewData['padData'])
-    return {
-        "predictions": pred,
-        "reviews": reviewData['reviews'],
-        "ratings": rawData['model']['ratings'],
-        "itemId": rawData["model"]['item']["itemId"],
-        "itemTitle": rawData["model"]['item']["itemTitle"],
-        "itemPic": rawData["model"]['item']["itemPic"],
-        "itemUrl": rawData["model"]['item']["itemUrl"]
-    }
+    # pred = model.predict(reviewData['padData'])
+    # return {
+    #     "predictions": pred,
+    #     "reviews": reviewData['reviews'],
+    #     "ratings": rawData['model']['ratings'],
+    #     "itemId": rawData["model"]['item']["itemId"],
+    #     "itemTitle": rawData["model"]['item']["itemTitle"],
+    #     "itemPic": rawData["model"]['item']["itemPic"],
+    #     "itemUrl": rawData["model"]['item']["itemUrl"]
+    # }
 
 @app.route('/api/v1/', methods=['POST'])
 def index():
     web_address = request.get_json()['web_address']
 
     result = detectFakeReviews(url=web_address)
-    result['predictions'] = np.array(result['predictions']).flatten().tolist()
-    result['reviews'] = np.array(result['reviews']).flatten().tolist()
-    res = {"result": result}
+    # result['predictions'] = np.array(result['predictions']).flatten().tolist()
+    # result['reviews'] = np.array(result['reviews']).flatten().tolist()
+    # res = {"result": result}
 
-    return jsonify(res)
+    return jsonify(result)
 
 
 
